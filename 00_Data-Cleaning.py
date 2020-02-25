7d0a9f98f8c30b9fdcbfcb17542b1ddf81ae28a8
BluEric1118 <ericlan1118@gmail.com>
2020-02-26 01:08:59 +0800

    Demo Case: Data Cleaning

---
+++ 00_Data-Cleaning.py
@@ -0,0 +1,49 @@
+﻿#Demonstration the following scenarios:
+#   1) Remove digit extensions, e.g. 1.00 -> 1
+#   2) Remove obs with strange column value 
+#   3) Format date column and create new column based on that
+
+def read_311_data(datafile):
+    import pandas as pd
+    import numpy as np
+    #Add the fix_zip function
+    def fix_zip(input_zip):
+        try:
+            input_zip = int(float(input_zip))
+        except:
+            try:
+                input_zip = int(input_zip.split('-')[0])
+            except:
+                return np.NaN
+        if input_zip < 10000 or input_zip > 19999:
+            return np.NaN
+        return str(input_zip)
+    
+    #Read the file
+    df = pd.read_csv(datafile,index_col='Unique Key')
+    
+    #fix the zip
+    df['Incident Zip'] = df['Incident Zip'].apply(fix_zip)
+    
+    #drop all rows that have any nans in them (note the easier syntax!)
+    
+    df = df.dropna(how='any')
+    
+    #get rid of unspecified boroughs
+    df = df[df['Borough'] != 'Unspecified']
+    
+    #Convert times to datetime and create a processing time column
+    
+    import datetime
+    df['Created Date'] = df['Created Date'].apply(lambda x:datetime.datetime.strptime(x,'%m/%d/%Y %I:%M:%S %p'))
+    df['Closed Date'] = df['Closed Date'].apply(lambda x:datetime.datetime.strptime(x,'%m/%d/%Y %I:%M:%S %p'))
+    df['processing_time'] =  df['Closed Date'] - df['Created Date']
+    
+    #Finally, get rid of negative processing times and return the final data frame
+    
+    df = df[df['processing_time']>=datetime.timedelta(0,0,0)]
+    
+    return df
+
+    df = read_311_data('nyc_311_data_subset.csv')
+    df.info()
